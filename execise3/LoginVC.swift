8c4caf47bf0bf2f840e80e65ae0401bfdb57c5aa
Mehmetdora <mehmetdora333@gmail.com>
2024-02-07 23:57:46 +0300

    düzenlemeler

--- execise3/LoginVC.swift
+++ execise3/LoginVC.swift
@@ -9,21 +9,84 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var questionL: UILabel!
+    @IBOutlet weak var altBaşlıkL: UILabel!
+    @IBOutlet weak var anaBaşlıkL: UILabel!
+    @IBOutlet weak var passWTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var editView: UIView!
+    @IBOutlet weak var LoginFirstL: UILabel!
+    @IBOutlet weak var loginOutlet: UIButton!
+    @IBOutlet weak var signInOutlet: UIButton!
+    @IBOutlet weak var açıklamaL: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        setGradientBackground()
+        hideKeyboard()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func hideKeyboard(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hide))
+        self.view.addGestureRecognizer(tapGesture)
     }
-    */
+    @objc func hide(){
+        self.view.endEditing(true)
+    }
+    
+    func setGradientBackground() {
+        
+        let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    func setupView(){
+        LoginFirstL.layer.borderWidth = 0
+        LoginFirstL.layer.cornerRadius = 15
+        LoginFirstL.layer.masksToBounds = true
+        editView.layer.borderWidth = 5
+        editView.layer.cornerRadius = 15
+        
+        emailTF.textColor = UIColor(named: "blackWhiteTextColor")
+        passWTF.textColor = UIColor(named: "blackWhiteTextColor")
+        emailTF.font = UIFont(name: "SpicyRice-Regular", size: 18.0)
+        passWTF.font = UIFont(name: "SpicyRice-Regular", size: 18.0)
+        emailTF.clearsOnBeginEditing = true
+        passWTF.clearsOnBeginEditing = true
+        loginOutlet.layer.cornerRadius = 10
+        signInOutlet.layer.cornerRadius = 10
+        passWTF.placeholder = "Password"
 
+        if Locale.current.language.languageCode == "tr"{
+            questionL.text = "Bir hesabın yok mu ?"
+            anaBaşlıkL.text = "Hadi Başlayalım"
+            altBaşlıkL.text = "Giriş"
+            emailTF.placeholder = "Email adresiniz"
+            passWTF.placeholder = "Şifreniz"
+            loginOutlet.setTitle("Giriş yap", for: .normal)
+            signInOutlet.setTitle("Kaydol", for: .normal)
+            açıklamaL.text = "Kendi hesabını oluşturarak tüm indirmlerden anında haberdar olabilir , kampanya ve fırsatlardan faydalanbilirsin."
+        }
+    }
+
+    @IBAction func signINButton(_ sender: Any) {
+        // Sign in screen code
+        
+    }
+    @IBAction func loginButton(_ sender: Any) {
+        // Login screen code
+    }
+    
+    @IBAction func exitButton(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
 }
--- execise3/ViewController.swift
+++ execise3/ViewController.swift
@@ -9,11 +9,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resim: UIImageView!
+    @IBOutlet weak var açıklamaL: UILabel!
+    @IBOutlet weak var AnaBaşlıkL: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var başlaOutlet: UIButton!
+    @IBOutlet weak var geçOutlet: UIButton!
+    @IBOutlet weak var geriOutlet: UIButton!
+    
+    let langs = Locale.current.language.languageCode!
+    var currentP = 0
+    let imageNames = ["image1","image2","image3","image4"]
+    let açıklamalarTR = ["Bütçene en uygun ürünleri sepetine ekleyebileceksin","Dilediğin her ürünü kolayca bulacaksın","Siparişlerin dakikalar içinde kapında olacak"]
+    let açıklamalarEN = ["You will be able to add the products that best suit your budget to your cart","You will easily find any product you want", "Your orders will be at your door within minutes"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setView()
+
+        
+    }
+    
+    func setView(){
+        let appearence = UINavigationBarAppearance()
+        appearence.titleTextAttributes = [.font : UIFont(name: "SpicyRice-Regular", size:28.0)]
+        appearence.backgroundColor = .orange
+        
+        navigationItem.standardAppearance = appearence
+        navigationItem.scrollEdgeAppearance = appearence
+        navigationItem.compactAppearance = appearence
+        
+        navigationItem.title = "GREENGROCERY"
+       
+        başlaOutlet.isHidden = true
+        pageControl.numberOfPages = 3
+        pageControl.currentPage = currentP
+        açıklamaL.font = UIFont(name: "SpicyRice-Regular", size:15.0)
+        açıklamaL.adjustsFontSizeToFitWidth = true
+        AnaBaşlıkL.font = UIFont(name: "SpicyRice-Regular", size: 22.0)
+        AnaBaşlıkL.adjustsFontSizeToFitWidth = true
+        resim.image = UIImage(named: imageNames[currentP])
+        
+        if langs == "en"{
+            açıklamaL.text = açıklamalarEN[currentP]
+        }else if langs == "tr"{
+            açıklamaL.text = açıklamalarTR[currentP]
+        }
+
+
     }
 
-
+    @IBAction func geçB(_ sender: Any) {
+        if (currentP<2){
+            currentP+=1
+            pageControl.currentPage = currentP
+            resim.image = UIImage(named: imageNames[currentP])
+            if langs == "en"{
+                açıklamaL.text = açıklamalarEN[currentP]
+            }else if langs == "tr"{
+                açıklamaL.text = açıklamalarTR[currentP]
+            }
+        }
+        if currentP == 2{
+            başlaOutlet.isHidden = false
+            geriOutlet.isHidden = true
+            geçOutlet.isHidden = true
+        }
+    }
+    
+    @IBAction func başlaB(_ sender: Any) {
+        // storyboard üzerinden bağlandığı için koda gerek yok
+    }
+    
+    @IBAction func geriB(_ sender: Any) {
+        if currentP>0{
+            currentP-=1
+            pageControl.currentPage = currentP
+            resim.image = UIImage(named: imageNames[currentP])
+            if langs == "en"{
+                açıklamaL.text = açıklamalarEN[currentP]
+            }else if langs == "tr"{
+                açıklamaL.text = açıklamalarTR[currentP]
+            }
+        }
+    }
+    
 }
 
